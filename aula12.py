e2e854dd7a535a50bae2e75281bb28e12a1361b1
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 09:29:13 -0300

    realizando exercicio imc

---
+++ aula12.py
@@ -0,0 +1,14 @@
+nome = 'Hellen'
+altura = '1.57'
+peso = 58
+imc = ... #Ellipsis (...) não gera erro 
+
+
+
+
+
+# Hellen tem 1.57 de altura,
+#pesa 58 quilos e seu IMC é 
+# 
+#o calcúlo do imc divide o peso em kg pela altura ao quadrado em metros
+#IMC = peso / (altura x altura)
