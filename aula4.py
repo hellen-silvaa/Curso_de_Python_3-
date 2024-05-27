eca107ab7579bb2caf14c64e76adb040131ca015
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 07:27:11 -0300

    testando tipos primitivos, números floats

--- aula4.py
+++ aula4.py
@@ -7,11 +7,11 @@ print(11)
 print(0)
 print(-11)
 
-
 #float -> Número com ponto flutuante
 #O tipo float representa qualquer número
 #positivo ou negativo com ponto flutuante.
 #float sem sinal é considerado positivo.
-
+print(1.1, 10.11)
+#Usa sempre '.' (ponto) para separar argumentos e vírgula usa pra outra coisa
 #A funçaõ type mostra o tipo que o Python
 #inferiu ao valor
