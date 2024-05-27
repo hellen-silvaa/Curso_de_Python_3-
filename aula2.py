cb3e5d033bc3e32e732e4675106357498850447f
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 07:03:18 -0300

    finalizando aula 2

--- aula2.py
+++ aula2.py
@@ -7,7 +7,14 @@ print(12, 34, 1011, sep='-', end='\r\n')
 print(56, 78, sep='_', end='\n')
 print(9, 10, 15, 60, sep='--', end='##\n')
 
-
 #argumentos não nomeados  varios argumentos para exibir
-
 #argumentos nomeados 
+
+''''
+     ----importante:----
+*Argumentos não nomeados (Positional Arguments): Passados na ordem definida.
+*Argumentos nomeados (Keyword Arguments): Passados usando o nome do parâmetro.
+*Mistura de argumentos: Os não nomeados vêm antes dos nomeados.
+*Argumentos com valores padrão: Usados se nenhum valor for fornecido.
+*Argumentos variáveis: *args para não nomeados e **kwargs para nomeados.
+*Ordem dos parâmetros: Posicionais, valores padrão, *args, nomeados, **kwargs.'''
