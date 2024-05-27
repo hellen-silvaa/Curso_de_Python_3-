e04ff06e2e70428480ba42a22ce58a19a2e821a9
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 06:58:23 -0300

    aprendendo print, quebra de linha

---
+++ aula2.py
@@ -0,0 +1,13 @@
+#passamos um argumento para o print
+#argumento é o conteudo dentro do print
+#\r \n -> CRLF
+#\n ->LF (quebra linha)
+
+print(12, 34, 1011, sep='-', end='\r\n')
+print(56, 78, sep='_', end='\n')
+print(9, 10, 15, 60, sep='--', end='##\n')
+
+
+#argumentos não nomeados  varios argumentos para exibir
+
+#argumentos nomeados 
