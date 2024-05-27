f8f535fa62d8df7b7cd24a92c616a1f0d0d2c3dd
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 07:43:25 -0300

    Aprendendo bool e testando

--- aula5.py
+++ aula5.py
@@ -5,3 +5,11 @@
 #Existem vários operadores para "questionar".
 #Dentre eles o ==, que é um operador lógico que
 #questiona se um valor é igual a outro
+print(10 == 10) #Sim => True (Verdadeiro)
+print(10 == 11) #Não => False (Falso)
+
+#Retornar o tipo de dado boolean
+print(type (True))
+print(type (False))
+print(type (10 == 10))
+print(type (10 == 11))
