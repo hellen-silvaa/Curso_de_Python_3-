5a59bb332c2a35819c1192c8184a3c34f1e598b6
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 08:01:59 -0300

    testando a coerção de tipos

---
+++ aula6.py
@@ -0,0 +1,13 @@
+#Conversão de tipos, coerção
+#type covertion, typecasting, coercion
+#é o ato de converter um tipo em outro
+#tipos imutáveis e primitivos:
+#str, int, float, bool
+print(1 + 1)
+print('a'+ 'b') #concatenação (apenas linguagem dinâmica)
+#print('1' + 1) #ele não consegue concatenar string com int
+
+
+#converção
+print(int('1'), type(int('1')))
+print(float('1.6') + 1)
