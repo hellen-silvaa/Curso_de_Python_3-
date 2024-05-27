3b796449a777554d5f0918ac25a60ffe908ae9a3
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 07:09:52 -0300

    diferença entre aspas

---
+++ aula3.py
@@ -0,0 +1,14 @@
+"""
+Python = Linguagem de Programação
+Tipo de tipagem = Dinâmica / Forte
+str ->string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+#Aspas simples 
+
+#Aspas duplas
+
+#Escape
+
+#r
