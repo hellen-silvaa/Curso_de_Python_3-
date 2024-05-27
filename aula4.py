9a3120ee46a7ad93acaf971dd3845bc55e758609
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 07:29:35 -0300

    testando função type

--- aula4.py
+++ aula4.py
@@ -1,5 +1,5 @@
 #Tipos Int e float
-#int -> Número inteiro
+#int -> Número inteiro 
 #O tipo int representa qualquer número
 #positivo ou negativo. int sem sinal é considerado
 #positivo.
@@ -7,11 +7,21 @@ print(11)
 print(0)
 print(-11)
 
-#float -> Número com ponto flutuante
+#float -> Número com ponto flutuante (se tem casa decimal)
 #O tipo float representa qualquer número
 #positivo ou negativo com ponto flutuante.
 #float sem sinal é considerado positivo.
 print(1.1, 10.11)
+print(0.0, -1.5)
 #Usa sempre '.' (ponto) para separar argumentos e vírgula usa pra outra coisa
 #A funçaõ type mostra o tipo que o Python
 #inferiu ao valor
+
+#pula varias linhas, porque não passou argumentos
+print(          )
+print(          )
+print(          )
+print(          )
+
+
+print(    type()      )
