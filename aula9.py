3a42f80c52ed79746aff1a714d8a9644c43f6e39
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 09:09:29 -0300

    testando

--- aula9.py
+++ aula9.py
@@ -7,8 +7,21 @@ print('Subtração:', subtracao)
 multiplicacao = 10 * 10
 print('Multiplicação:', multiplicacao)
 
-divisao = 10 / 2.2
+divisao = 10 / 3 #float 
 print('Divisão:', divisao)
 
-divisao_inteira = 10 // 2.2
+divisao_inteira = 10 // 3 # corta as casas decimais
 print('Divisão inteira: ', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação: ', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo: ', modulo)
+#saber se um número é multiplo de outro, se dividir e o resto for 0 ele é multiplo do número
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
