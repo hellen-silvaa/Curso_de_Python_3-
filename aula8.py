0a8de334e704643e89dab72d191d1d15c8ae63ca
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 08:52:32 -0300

    conferindo resolução do professor

--- aula8.py
+++ aula8.py
@@ -2,10 +2,25 @@
 nome = 'Hellen'
 sobrenome = 'Silva'
 idade = 21
-ano_nascimento = 2002
+ano_nascimento = 2024 - idade
 maior_de_idade = idade >= 18
 altura_metros = 1.57
 
+
+print('Nome:' , nome )
+print('Sobrenome:' , sobrenome)
+print('Idade:' , idade)
+print('Ano de nascimento:' , ano_nascimento)
+print('É maior de idade? ' , maior_de_idade)
+print('Altura em metros:' , altura_metros)
+
+#Resolução professor:
+nome = 'Luiz Otávio'
+sobrenome = 'Miranda'
+idade = 30
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.80
 
 print('Nome:' , nome )
 print('Sobrenome:' , sobrenome)
