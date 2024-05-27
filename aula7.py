44cd3c8ed7c84db841ddc485001d6d4f0a95664a
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 08:31:46 -0300

    iniciando introdução às variáveis

---
+++ aula7.py
@@ -0,0 +1,7 @@
+#PEP 8 => guia de estilos do código Python
+#Variáveis são usadas para salvar algo na memória do computador
+#PEP8: inicie variáveis com letras mminúsculas, pode usar 
+#números e underline _.
+#O sinal de = é o operador de atribuição. Ele é usado para
+#atribuir um valor a um nome (variável).
+#Uso: nome_variavel = expressão
