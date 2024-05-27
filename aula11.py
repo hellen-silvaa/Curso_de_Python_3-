7109a03d4187ab7510a61e0b5c716df1fee11d50
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 09:22:41 -0300

    Precedência entre os operadores aritméticos

---
+++ aula11.py
@@ -0,0 +1,13 @@
+#o interpretador lê o código da esquerda para a direita
+#de cima para baixo
+#1. (n + n)
+#2. **
+#3. * / // %
+#4. + -
+conta_1 = 1+ 1 ** 5 + 5 #7
+conta_1 = 'Qualquer outra coisa'
+print(conta_1)
+
+conta_2 = (1+1) ** (5 + 5) #1024
+print(conta_2)
+
