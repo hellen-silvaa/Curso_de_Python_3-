93def5fc81468422642663c1fa33ec93aa3de34b
Hellen Moura <hellen.aparecidams@outlook.com>
2024-05-27 06:59:49 -0300

    sep, end, \n, argumentos

--- aula2.py
+++ aula2.py
@@ -2,7 +2,7 @@
 #argumento é o conteudo dentro do print
 #\r \n -> CRLF
 #\n ->LF (quebra linha)
-
+#Print != print (Prirnt com letra maiuscula não existe no python apenas com letra minuscula)
 print(12, 34, 1011, sep='-', end='\r\n')
 print(56, 78, sep='_', end='\n')
 print(9, 10, 15, 60, sep='--', end='##\n')
