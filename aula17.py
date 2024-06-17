d1d02eeffb5c35453af8fc2f8d42dc99c473ba19
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-17 17:37:26 -0300

    testando código

--- aula17.py
+++ aula17.py
@@ -3,16 +3,26 @@
 #pass ou ... -> passa aquele trecho de código que não foi escrito no momento
 condicao1 = False
 condicao2 = False
-condicao3 = False
+condicao3 = True
 condicao4 = False
+# se condicao 1 for verdadeira execute este trecho de código
 if condicao1:
     print('Código para condição 1')
+    print('Código para condição 1')
+# se não se
 elif condicao2:
-    pass
+    print('Código para condição 2')
+#Ao encontrar a condicao 3 como verdadeira ele executa e sai do blobo
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+# se não
 else:
     print('Nenhuma condição foi satisfeita')
 
-    if 10 ==10:
+#fora do bloco
+if 10 ==10:
         print('Outro if')
 
 
