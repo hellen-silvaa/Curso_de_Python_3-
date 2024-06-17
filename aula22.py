77877a37afdac3c26388550c4eb5c14bc3768b02
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-17 19:50:13 -0300

    operador lógico' or'

---
+++ aula22.py
@@ -0,0 +1,17 @@
+# if 0 and 1:
+#     print(True and 1)
+# #O 0 (zero) de 0 and 1 avalia como falsy. O corpo do if não será executado.
+# if 1 and 1:
+#     print(True and 1 and False)
+#     #false
+
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# or - Qualquer condição verdadeira avalia
+# toda a expressão como verdadeira.
+# Se qualquer valor for considerado verdadeiro,
+# a expressão inteira será avaliada naquele valor.
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
