326cc8df5f87e700d6022285e7fcd00bcd597a90
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-17 19:18:16 -0300

    finalizando

---
+++ aula19.py
@@ -0,0 +1,22 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado      Esemplo (True)
+>       maior            2 > 1
+>=      maior ou igual   2 >= 2
+<       menor            1 < 2
+<=      menor ou igual   2 <= 2
+==      igual            'a' == 'a'
+!=      diferente        'a' != 'b'
+=       atribuição       recebe um valor
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2>= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print('Olha meu print')
+
+# python -i aula_19.py carrega o modulo no powershell interativo
+# quit () -> sai
