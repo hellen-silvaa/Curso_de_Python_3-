f25b566070680ef60a9bfdcb666c5063e0861328
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-17 17:34:19 -0300

    aprendendo pass e ...

---
+++ aula17.py
@@ -0,0 +1,19 @@
+# if /    elif   / else
+#se  / se não se / se não
+#pass ou ... -> passa aquele trecho de código que não foi escrito no momento
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+if condicao1:
+    print('Código para condição 1')
+elif condicao2:
+    pass
+else:
+    print('Nenhuma condição foi satisfeita')
+
+    if 10 ==10:
+        print('Outro if')
+
+
+
