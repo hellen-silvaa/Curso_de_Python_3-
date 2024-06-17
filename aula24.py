cced4c49c77dd3617f8dad569e773f169ada1e51
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-17 20:46:58 -0300

    teste

--- aula24.py
+++ aula24.py
@@ -1,13 +1,25 @@
 # Operadores in e not in
-# Strings são iteráveis
+# Strings são iteráveis (navega item por item)
 #  0 1 2 3 4 5
-#  O t á v i o
+#  H e l l e n
 # -6-5-4-3-2-1
-# nome = 'Otávio'
-# print(nome[2])
-# print(nome[-4])
-# print('vio' in nome)
+# in -> entre (esta entre)
+# not in -> não entre (não esta entre)
+# para acesar o indice usamos [ ]
+
+# nome = 'Hellen'
+# print(nome [1])
+# print(nome [-5])
+# print('e' in nome)
 # print('zero' in nome)
-# print(10 * '-')
-# print('vio' not in nome)
+# print(10* '-')
+# print('e' not in nome)
 # print('zero' not in nome)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+        print(f'{encontrar} não está em {nome}')
