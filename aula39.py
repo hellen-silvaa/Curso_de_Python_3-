6e9e69d7d91c2a4cadade664c20f2a1304522b5e
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 10:40:42 -0300

    tentando

---
+++ aula39.py
@@ -0,0 +1,14 @@
+"""
+Integrando strings com while
+"""
+#        01234567891011               indices positivos
+nome =  'Hellen Silva'  #Iteráveis
+#        11109876543210               indices negativos
+
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3])            #buscando a letra do indice 3
+
+#proximo exercicio unir as coisas
+#nova_string += '*H*e*l*l*e*n**S*i*l*v*a*''
