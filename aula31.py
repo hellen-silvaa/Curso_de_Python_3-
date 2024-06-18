ffcc72a53b411a920ba91b7d2cab227d5674b153
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 07:27:28 -0300

    testando

---
+++ aula31.py
@@ -0,0 +1,20 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+# com id o python busca a identidade do valor na memoria
+# v1 = 'a'
+# v2 = 'b'
+# print(id(v1))
+# print(id(v2))
+
+condicao = False
+
+if condicao:
+   print('Faça algo')
+else:
+   print('Não faça algo')
+
