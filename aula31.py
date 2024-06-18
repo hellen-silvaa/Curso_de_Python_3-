3d7d76a6934dca5636d048cac75677360a3f90d2
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 07:43:47 -0300

    testando is not none

--- aula31.py
+++ aula31.py
@@ -21,6 +21,13 @@ if condicao:
 else:
    print('Não faça algo')
 
-print(passou_no_if, passou_no_if is None)
-print(passou_no_if, passou_no_if is not None)
 
+# print(passou_no_if, passou_no_if is None)
+# print(passou_no_if, passou_no_if is not None)
+
+
+if passou_no_if is None:
+    print('Não passou no if')
+
+if passou_no_if is not None:
+    print('Passou no if')
