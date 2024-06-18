f30b4720271c0857863e1321aa4fe2b66fad748f
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 07:17:06 -0300

    deixando código simples e limpo

--- aula30.py
+++ aula30.py
@@ -7,17 +7,25 @@ Muitas condições no mesmo if (ruim)
 """
 
 velocidade = 61 #velocidade atual do carro
-local_carro = 101 # local em que o carro está na estrada
+local_carro = 100 # local em que o carro está na estrada
 
 RADAR_1 = 60 #velocidade máxima do radar 1
 LOCAL_1 = 100 #local onde o radar 1 está
 RADAR_RANGE = 1 #A distância onde o radar pega
 
+#criar variavel do que esta repetindo para deixar o código limpo
+velocidade_carro_passou_RADAR1 = velocidade > RADAR_1
+
+if velocidade_carro_passou_RADAR1:
+    print('Velocidade do carro passou do radar 1')
 
 
+#saber se foi ou não multado
 
-
-
+if local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+        local_carro<= (LOCAL_1 + RADAR_RANGE) and \
+            velocidade_carro_passou_RADAR1:
+    print('Carro multado em radar 1')
 
 
 
