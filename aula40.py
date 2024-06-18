707ffdb759e0cfff4e8c9e20718dcf502a9da1ac
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 10:58:19 -0300

    iniciando calculadora

---
+++ aula40.py
@@ -0,0 +1,15 @@
+"""Calculadora com while"""
+#lower converte maiusculo para minusculo
+while True:
+    print('nummmmm')
+    #########
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
+
+
+
+
+
+
