8d24a57e6a4a72285e6ab26c0cd310608d759720
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 06:56:29 -0300

    Isgifit

--- aula29.py
+++ aula29.py
@@ -2,4 +2,19 @@
 Introdução ao try/except
 try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
+input sempre retorna string
 """
+
+numero_str = input('Vou dobrar o número que você digitar: ')
+
+#SEMPRE QUE O USUARIO MANDA ALGO VOCÊ PRECISA TRATAR ESSE DADO
+#transformando string digitada para número flutuante
+
+#isdigit -> verifica se usuario digitou APENAS NÚMEROS True or False
+# print(numero_str.isdigit())
+
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso não é um número')
