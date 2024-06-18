5ea082b424488e469f6d76b4f33b249cd2e56bb6
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 07:00:19 -0300

    muito legal try and except

--- aula29.py
+++ aula29.py
@@ -5,7 +5,7 @@ except -> ocorreu algum erro ao tentar executar
 input sempre retorna string
 """
 
-numero_str = input('Vou dobrar o número que você digitar: ')
+#numero_str = input('Vou dobrar o número que você digitar: ')
 
 #SEMPRE QUE O USUARIO MANDA ALGO VOCÊ PRECISA TRATAR ESSE DADO
 #transformando string digitada para número flutuante
@@ -18,3 +18,16 @@ numero_str = input('Vou dobrar o número que você digitar: ')
 #     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 # else:
 #     print('Isso não é um número')
+
+
+numero_str = input('Vou dobrar o número que você digitar: ')
+
+try:
+    print('STR: ', numero_str)
+    numero_float = float(numero_str)
+    print('FLOAT: ', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
+
+# try tenta executar e se ocorrer um erro dentro do try ele pula pro except
