4e5a459ba32876caa4776a81a8d96e758ed9e946
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 09:01:29 -0300

    nao podemos mudar os valores das variaveis é melhor criar outra variavel

--- aula33.py
+++ aula33.py
@@ -2,3 +2,13 @@
 https://docs.python.org/pt-br/3/library/stdtypes.html
 Imutáveis que vimos: str, int, float, bool
 """
+
+#este valor é imutável
+string = 'Maria'
+outra_variavel = f'{string[:2]}'
+print(string[2])
+
+
+
+#você não pode mudar o valor
+# string[3] = 'ABC'
