96f28efb6b2120e24129421cd454caff0ece1293
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 09:59:35 -0300

    exemplo 1 realizado

--- aula34.py
+++ aula34.py
@@ -4,3 +4,22 @@ while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 Loop infinito -> Quando um código não tem fim
 """
+#Loop infinito
+# condicao = True
+# while condicao:
+#     print(1)
+#     print(2)
+#     print(3)
+
+
+condicao = True
+ 
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou')
+#break busca o laço mais proximo dele e sai do laço while 
