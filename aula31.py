6888d0c9234869172d9109636c89d0f5f44fd46b
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 07:46:18 -0300

    testando

--- aula31.py
+++ aula31.py
@@ -12,7 +12,7 @@ id = Identidade
 # print(id(v2))
 
 #declarar a variavel fora do bloco para usar ela dentro do bloco
-condicao = True
+condicao = False
 passou_no_if = None
 
 if condicao:
@@ -21,7 +21,6 @@ if condicao:
 else:
    print('Não faça algo')
 
-
 # print(passou_no_if, passou_no_if is None)
 # print(passou_no_if, passou_no_if is not None)
 
@@ -29,5 +28,8 @@ else:
 if passou_no_if is None:
     print('Não passou no if')
 
-if passou_no_if is not None:
+# if passou_no_if is not None:
+#     print('Passou no if')
+#or
+else:
     print('Passou no if')
