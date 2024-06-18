f4d1891dcfca2a5f81a4383849662951ef38858c
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-18 10:47:03 -0300

    consegui

--- aula39.py
+++ aula39.py
@@ -1,14 +1,26 @@
 """
 Integrando strings com while
 """
-#        01234567891011               indices positivos
-nome =  'Hellen Silva'  #Iteráveis
-#        11109876543210               indices negativos
+# #        01234567891011               indices positivos
+# nome =  'Hellen Silva'  #Iteráveis
+# #        11109876543210               indices negativos
 
-tamanho_nome = len(nome)
-print(nome)
-print(tamanho_nome)
-print(nome[3])            #buscando a letra do indice 3
+# tamanho_nome = len(nome)
+# print(nome)
+# print(tamanho_nome)
+# print(nome[3])            #buscando a letra do indice 3
 
 #proximo exercicio unir as coisas
 #nova_string += '*H*e*l*l*e*n**S*i*l*v*a*''
+
+nome = 'Hellen Silva'
+
+indice = 0
+novo_nome = ''
+while indice <len(nome):
+    letra = (nome[indice])
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
