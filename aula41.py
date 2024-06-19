154f283bee55f2ac90a633afaa8f26a7996551e2
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-19 16:43:28 -0300

    testando

--- aula41.py
+++ aula41.py
@@ -1,9 +1,18 @@
 """while/ellse"""
+#i -> variavel i para representar o indice e contar indice 
 string = 'Valor qualquer'
 
 i = 0
 while i < len(string):
     letra = string[i]
 
+    if letra == ' ':
+        break
+
     print(letra)
     i += 1
+else: 
+    print('Não encontrei um espaço na string')
+print('Fora do while')
+#break encontrado já pula fora do while sem executar else
+#se não tem break executa while
