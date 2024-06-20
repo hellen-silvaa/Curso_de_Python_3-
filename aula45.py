762242194cd31da631e8aee8d1bcdf4b30428467
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-20 16:17:13 -0300

    Como o for funciona por baixo dos panos? (next, iter, iterável e iterador)

---
+++ aula45.py
@@ -0,0 +1,13 @@
+"""
+Iterável -> str, range, etc
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+For + Range
+range -> range(start,stop,step)
+"""
+
+numeros = range(0,100,8)
+
+for numero in numeros:
+    print(numero)
