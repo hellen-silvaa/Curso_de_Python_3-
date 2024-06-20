76cc89385d9991dd2eee255050baefca162566be
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-20 17:37:10 -0300

    Tipo list - Introdução às listas mutáveis em Python

---
+++ aula48.py
@@ -0,0 +1,14 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+len -> verifica qtd de caractere
+"""
+#listas conseguimos acessar elementos por elementos
+#        +01234
+#        -54321
+string = 'ABCDE'  # 5 caracteres (len)
+# print(bool([]))  # falsy
+# print(lista, type(lista))
