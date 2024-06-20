ec5a1d3c51b9e0e48057c63567d89a53c28af8de
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-20 17:33:04 -0300

    Deu certo!!!

--- aula47.py
+++ aula47.py
@@ -15,10 +15,16 @@ Faça a contagem de tentativas do seu
 usuário.
 """
 
+import os
+
 palavra_secreta = 'perfume'
 letras_acertadas = ''
+numero_tentativa = 0
+
 while True:
+
     letra_digitada = input ('Digite uma letra: ')
+    numero_tentativa += 1
 
     if len(letra_digitada)>1:
         print('Digite apenas uma letra.')
@@ -28,10 +34,24 @@ while True:
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada #concatenação
 
+    palavra_formada = ''
     for letra_secreta in palavra_secreta:
         #se a letra secreta estiver em letras acertadas exiba a letra 
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
+             palavra_formada += letra_secreta
             #se a letra naõ estiver vai exibir o *
         else:
-            print('*')
+            palavra_formada += '*'
+    print(palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        #limpa a tela e volta no começo
+        os.system('cls')
+        print('VOCÊ GANHOU, PARABÉNS!!!! ')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas:', numero_tentativa)
+        #volta pro começo do jogo zerado
+        letras_acertadas = ''
+        numero_tentativa = 0
+
+
