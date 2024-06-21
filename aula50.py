fee791cf531186c02d2080808643e60a43980f2c
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-21 09:50:41 -0300

    exercicio

---
+++ aula50.py
@@ -0,0 +1,14 @@
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Hellen', 'Helena', 'Maria']
+lista.append('João')
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
+
