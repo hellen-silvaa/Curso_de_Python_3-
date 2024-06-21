7925143d3549f6d931b884039fd0d875b43ee7e6
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-21 10:04:48 -0300

    testando

--- aula53.py
+++ aula53.py
@@ -4,3 +4,8 @@ enumerate - enumera iteráveis (índices)
 enumerate para enumerar valores de iteráveis (pegar índices)
 """
 
+lista = ['Hellen', 'Helena', 'Maria']
+lista.append('João')
+
+lista_enumerada = enumerate(lista)
+print(lista_enumerada)
