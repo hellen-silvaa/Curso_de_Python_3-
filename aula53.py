1043432b9e85a00e195888eb123fd2640c762695
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-21 10:12:48 -0300

    aplicando boas práticas

--- aula53.py
+++ aula53.py
@@ -2,10 +2,23 @@
 enumerate - enumera iteráveis (índices)
 
 enumerate para enumerar valores de iteráveis (pegar índices)
+
+
+[(0, 'Hellen'), (1, 'Helena'), (2, 'Maria'), (3, 'João')]
 """
 
 lista = ['Hellen', 'Helena', 'Maria']
 lista.append('João')
 
-lista_enumerada = enumerate(lista)
-print(lista_enumerada)
+# lista_enumerada = list(enumerate(lista))
+# print(lista_enumerada)
+
+
+for item in enumerate(lista):
+    indice, nome = item 
+    print(indice, nome)
+
+#esse é a mesma coisa do de cima,porém bemm mais simplificado
+#código limpo    
+for indice, nome in enumerate(lista):
+    print(indice, nome)
