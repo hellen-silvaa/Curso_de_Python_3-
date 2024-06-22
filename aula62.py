06a92d19d8d1fac94f6ba8fb1ba1b1735b7140ed
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-22 15:26:49 -0300

    finalizando digito 2

--- aula62.py
+++ aula62.py
@@ -25,4 +25,43 @@ contrário disso:
 O segundo dígito do CPF é 0
 """
 # cpf = '36440847007'  # Esse CPF gera o primeiro dígito como 10 (0)
-cpf = '74682489070'
+#----------------------------------------------------------------------------------------
+#CALCULAR PRIMEIRO DIGITO
+
+cpf_enviado_usuario = '74682489070'
+nove_digitos = cpf_enviado_usuario [:9] #fatiamento do indice zero ao nove
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+#para cada digito nos 9 digitos faça:
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+#se meu digito_1 for igual a digito_1 se digito_1 for menor que 9 ao contrario será 0
+digito_1 = digito_1 if digito_1 <= 9 else 0
+print(digito_1)
+
+#----------------------------------------------------------------------------------------
+#CALCULAR SEGUNDO DIGITO
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+print(digito_2)
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+print(cpf_gerado_pelo_calculo)
+
+#verificar se é um cpf valido
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é valido!')
+else:
+    print('CPF inválido!')
