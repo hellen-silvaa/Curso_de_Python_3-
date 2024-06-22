ee50f9faccf4aa8ecd0e2dea14654cda5b38990f
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-22 07:04:18 -0300

    resolvendo de uma outra forma

--- aula_54.py
+++ aula_54.py
@@ -6,11 +6,14 @@ Não permita que o programa quebre com
 erros de índices inexistentes na lista.
 """
 
-#                     0       1        2         3
-lista_de_compra = ['maça', 'leite', 'arroz', 'macarrão']
-#inserir
-lista_de_compra.append('suco de maracujá') #passa a ser o indice 4 e ao apagar o leite ele passa a ser o 3
-#apagar
-del lista_de_compra [1] #vamos apagar o leite da nossa lista
-#listar valores da lista
-print(lista_de_compra)
+# #                     0       1        2         3
+# lista_de_compra = ['maça', 'leite', 'arroz', 'macarrão']
+
+# #inserir
+# lista_de_compra.append('suco de maracujá') #passa a ser o indice 4 e ao apagar o leite ele passa a ser o 3
+
+# #apagar
+# del lista_de_compra [1] #vamos apagar o leite da nossa lista
+
+# #listar valores da lista
+# print(lista_de_compra)
