6c26cde600ddabc316a9196aab846f1297d6a9d7
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-22 14:44:47 -0300

    tentando solucionar exercicio cpf, dificil

--- aula61.py
+++ aula61.py
@@ -12,12 +12,16 @@ Ex.:  746.824.890-70 (746824890)
 
 Somar todos os resultados: 
 70+36+48+56+12+20+32+27+0 = 301
+
 Multiplicar o resultado anterior por 10
 301 * 10 = 3010
+
 Obter o resto da divisão da conta anterior por 11
 3010 % 11 = 7
+
 Se o resultado anterior for maior que 9:
     resultado é 0
+    
 contrário disso:
     resultado é o valor da conta
 
