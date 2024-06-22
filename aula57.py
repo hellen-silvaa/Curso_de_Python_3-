7a3aabc9d55ba39dd7ae263f8cad6a4f502dae18
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-22 14:10:23 -0300

    lista dentro de lista

--- aula57.py
+++ aula57.py
@@ -7,15 +7,15 @@ salas = [
     # 0
     ['Elaine', ],  # 1
     # 0       1       2
-    ['Ruan', 'João', 'Eduarda', ],  # 2
+    ['Ruan', 'João', 'Eduarda', (0,10,20,30,40)],  # 2
 ]
 
-# print(salas[1][0])
+#print(salas[2][3][3])
 # print(salas[0][1])
 # print(salas[2][2])
 # print(salas[2][3][3])
 
 for sala in salas:
-    print(f'A sala é {sala}')
-    for aluno in sala:
-        print(aluno)
+     print(f'A sala é {sala}')
+     for aluno in sala:
+         print(aluno)
