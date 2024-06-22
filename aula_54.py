5f40bcee883a3905353b66c380bacc1888a3a43e
Hellen Moura <hellen.aparecidams@outlook.com>
2024-06-22 06:59:31 -0300

    lista ok

--- aula_54.py
+++ aula_54.py
@@ -6,3 +6,6 @@ Não permita que o programa quebre com
 erros de índices inexistentes na lista.
 """
 
+#                     0       1        2         3
+lista_de_compra = ['maça', 'leite', 'arroz', 'macarrão']
+print(lista_de_compra)
