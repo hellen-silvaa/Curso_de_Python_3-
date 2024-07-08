e0e9f376564bf8606c2711ed41421d153e00888b
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-08 18:11:32 -0300

    escopo e modulos

--- aula68.py
+++ aula68.py
@@ -6,3 +6,23 @@ O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
 """
+
+x = 1
+
+#corpo da função onde definimos
+def escopo():
+    global x
+    x = 10 
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x,y)
+    outra_funcao()
+    print(x)
+
+print(x) # valor da variavel protegida
+#aqui executamos o que foi definido
+escopo()
+print(x) #valor da primeira variavel
