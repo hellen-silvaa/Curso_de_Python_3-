baf6b35cfd12790e3ae1e26acb6d4cee6a720253
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-09 07:55:13 -0300

    Closure e funções que retornam outras funções

--- aula73.py
+++ aula73.py
@@ -17,3 +17,10 @@ print(
 print(
     executa(saudacao, 'Boa noite', 'Maria')
 )
+
+# Termos técnicos: Higher Order Functions e First-Class Functions
+# Academicamente, os termos Higher Order Functions e First-Class Functions têm significados diferentes.
+
+# Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+# First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
