888aa94e4470c12e977cfc796e44b72c380d545a
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-09 07:22:54 -0300

    resolvendo

--- aula72.py
+++ aula72.py
@@ -5,7 +5,11 @@
 # Retorne o total para uma variável e mostre o valor
 # da variável.
 
-
+def multiplicacao (*args):
+    total = 0
+    for numero in args:
+        total *= numero
+        print(total)
 
 
 # Crie uma função fala se um número é par ou ímpar.
