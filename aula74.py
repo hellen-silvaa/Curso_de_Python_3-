8162dccb185ebf5332cb65e476f7b7a3789d0993
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-09 08:02:47 -0300

    Closure e funções que retornam outras funções

--- aula74.py
+++ aula74.py
@@ -1,3 +1,16 @@
 """
 Closure e funções que retornam outras funções
 """
+
+
+def criar_saudacao(saudacao, nome):
+    def saudar():
+        return f' {saudacao}. {nome}'
+    return saudar
+
+
+s1 = criar_saudacao ('Bom dia' , 'Hellen')
+s2 = criar_saudacao ('Bom dia' , 'Ruan')
+
+print(s1)
+print(s2)
