63776db1142645bf30d1ca261487382c8d8fa402
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-09 07:11:05 -0300

    empacotando e desempacotando

--- aula71.py
+++ aula71.py
@@ -16,10 +16,23 @@ def soma (*args):
         print('Total', total,'+', numero)
         total += numero
         print('Total', total)
-    print(total)
+    return total
     #passando args para lista
     # args = list(args)
     # print(args, type(args))
 
 
-soma(1, 2, 3, 4, 5, 6)
+soma_1_2_3 = soma(1, 2, 3)
+print(soma_1_2_3)
+
+soma_4_5_6 = soma(1, 2, 3, 4, 5, 6)
+print(soma_4_5_6)
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros) #*numeros -> desempacotando
+print(outra_soma)
+
+#sum realiza a soma
+print(sum((numeros)))
+
+# print(*numeros)
