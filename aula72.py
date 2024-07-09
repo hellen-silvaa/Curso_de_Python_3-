8b287d6bd25b9e527d193924ee380e654dfb97f1
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-09 07:33:35 -0300

    resolvido o primeiro

--- aula72.py
+++ aula72.py
@@ -11,8 +11,29 @@ def multiplicar(*args):
         total *= numero
     return total
 
-multiplicacao = multiplicar (1, 2, 3, 4, 5, 6)
+multiplicacao = multiplicar (10, 2, 3, 4, 5, 6)
 print(multiplicacao)
 
 # Crie uma função fala se um número é par ou ímpar.
 # Retorne se o número é par ou ímpar.
+
+# def par_impar (numero):
+#     return numero % 2 == 0
+
+# print(par_impar(2))
+# print(par_impar(3))
+# print(par_impar(15))
+# print(par_impar(16))
+
+def par_impar (numero):
+    multiplo_de_dois = numero % 2 == 0
+
+    if multiplo_de_dois:
+        return f'{numero} é par'
+    else:
+        return f'{numero} é ímpar'
+
+print(par_impar(2))
+print(par_impar(3))
+print(par_impar(15))
+print(par_impar(16))
